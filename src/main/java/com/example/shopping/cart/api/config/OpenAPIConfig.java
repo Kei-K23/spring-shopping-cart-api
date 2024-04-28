e85c995587b7a09d6d15cf2d7c7fa6dc63e9aee1
Kei-K23 <keiksl2301@gmail.com>
2024-04-28 18:35:58 +0630

    Add authorization header input to Swagger UI

--- src/main/java/com/example/shopping/cart/api/config/OpenAPIConfig.java
+++ src/main/java/com/example/shopping/cart/api/config/OpenAPIConfig.java
@@ -1,13 +1,16 @@
 package com.example.shopping.cart.api.config;
 
-import java.util.List;
+import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
@@ -22,11 +25,21 @@ public class OpenAPIConfig {
         myContact.setName("Arkar Min");
         myContact.setEmail("arkar1712luffy@gmail.com");
 
-        Info information = new Info()
-                .title("Shopping Cart with products management API")
-                .version("1.0")
-                .description("This API exposes endpoints to manage products and shopping carts.")
-                .contact(myContact);
-        return new OpenAPI().info(information).servers(List.of(server));
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .description("Authorization header using the Bearer scheme");
+
+        // Define the OpenAPI document
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Shopping Cart with products management API")
+                        .version("1.0")
+                        .description("This API exposes endpoints to manage products and shopping carts.")
+                        .contact(myContact))
+                .servers(Arrays.asList(server))
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme));
+
     }
 }
